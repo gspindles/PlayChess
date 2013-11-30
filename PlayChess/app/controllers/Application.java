1e8ab6c60e305a80953d0fa5d45f7a85cfe9fdf9
gspindles <joseph.m.ching@gmail.com>
2013-11-30 17:05:30 -0600

    added highlighting upon click

--- PlayChess/app/controllers/Application.java
+++ PlayChess/app/controllers/Application.java
@@ -2,19 +2,28 @@ package controllers;
 
 import play.*;
 import play.mvc.*;
-
+import play.libs.*;
+import chessjava.*;
 import views.html.*;
+import play.Logger.*;
+import com.fasterxml.jackson.core.*;
+import com.fasterxml.jackson.databind.*;
 
 public class Application extends Controller {
 
     public static Result index() {
-    	
-    	
+    	ChessGame chessGame = new ChessGame();
         return ok(index.render("Your new application is ready."));
     }
 
-    public static Result getPossibleMoves() {
-    	return ok();
+    public static Result getPossibleMoves(int x, int y) {
+        // temporary toy for now
+        Logger.info("x:" + x + " y: " + y);
+        Point p = new Point(x, y);
+        Point[] ps = new Point[3];
+        ps[0] = new Point(p.x, p.y - 1);
+        ps[1] = new Point(p.x + 1, p.y - 1);
+        ps[2] = new Point(p.x - 1, p.y - 1);
+    	return ok(Json.toJson(ps));
     }
-
 }
