0eeeff8aa4f1906e82c98750496034e5e43d910a
Carey Wilson <carey@wilsonaire.com>
2013-12-09 17:44:43 -0600

    Created sequential and parallel methods for aiMakeATurn... untested.

--- PlayChess/app/models/ChessGame.java
+++ PlayChess/app/models/ChessGame.java
@@ -2,6 +2,7 @@ package models;
 
 import java.util.List;
 import java.util.ArrayList;
+import java.util.concurrent.*;
 
 public class ChessGame {
 
@@ -27,7 +28,47 @@ public class ChessGame {
 
 
 	// This method tells ai to make a turn and apply it to the board permanently
-	public static void aiMakeATurn() {
-
+	public List<Move> aiMakeATurnParallel(Side side) throws InterruptedException, ExecutionException {
+		//Tracks progress of 8 asynchronous threads.
+		ExecutorService pool = Executors.newFixedThreadPool(8);
+		//A set of Future objects, which manage returned values from the callable threads.
+		Set<Future<Move>> set = new HashSet<Future<Move>>();
+		
+		//Using threads, simultaniously scan through each column tallying a list of possible moves.
+		for(int i = 0; i < 8; i++) {
+			//Create threads of ParallelProcessing. Accepts a List<Move> object.
+			Callable<List<Move>> myCallableThread = new ParallelProcessing(i, this.chessBoard, side);
+			//pool.submit(thread) returns a "future" object containing a list of moves from that collumn.
+			Future<List<Move>> future = pool.submit(myCallableThread);
+			//Add future object (which contains List<Move> to our set of thread return values.
+			set.add(future);
+		}
+		
+		//Transition Set to List
+		List<Move> availableMoves = new ArrayList<Move>();
+		//Iterate through all the future objects in our HashSet.
+		for(Future<Integer> futureMoveList : set) {
+			//List.addAll(collection) adds all the elments of a collection, in this case, all the elements of a List<Move>
+			//contained within each future object.
+			//Future.get() will block then return a List<Move> when it's thread is finished processing.
+			availableMoves.addAll(futureMoveList.get());
+		}
+		
+		return availableMoves;
+	}
+	
+	public List<Move> aiMakeATurnSequential(Side side) {
+		//Since ParallelProcessing as the necessary code to search for possible moves... Use that!
+		ParallelProcessing sequentialMoveSearch = new ParallelProcessing();
+		
+		List<Move> availableMoves = new ArrayList<Move>();
+		
+		//Sequentially iterate through each collumn and tally a list of possible moves.
+		for(int i = 0; i < 8; i++) {
+			//Add all the elements from the returned collection of sequentialMoveSearch to the main list, availableMoves.
+			availableMoves.addAll(sequentialMoveSearch.populate(i, this.chessBoard, side));
+		}
+	
+		return availableMoves;
 	}
 }
