1073223c305616785226880df5e2bc0ff518d03b
gspindles <joseph.m.ching@gmail.com>
2013-12-10 00:27:59 -0600

    will probably switch back to drag and drop

--- PlayChess/app/controllers/Application.java
+++ PlayChess/app/controllers/Application.java
@@ -10,6 +10,7 @@ import com.fasterxml.jackson.core.*;
 import com.fasterxml.jackson.databind.*;
 
 import java.util.List;
+import java.util.ArrayList;
 
 public class Application extends Controller {
     public static ChessGame chessGame;
@@ -23,7 +24,51 @@ public class Application extends Controller {
 
 
     public static Result getPossibleMoves(int x, int y) {
-        List<Point> ps = chessGame.getPossibleMoves(x, y);
+        Logger.info("x:" + x + " y: " + y);
+        /*List<Point> ps = new ArrayList<Point>();
+        ps = chessGame.getPossibleMoves(x, y);
+        if (ps == null) { Logger.info("Empty list\n"); }
+        for (Point p : ps) { Logger.info("Point : " + x + " " + y + "\n" ); }*/
+        Point p = new Point(x, y);
+        Point[] ps = new Point[8];
+        ps[0] = new Point(p.x + 1, 7 - p.y - 1);
+        ps[1] = new Point(p.x, 7 - p.y - 1);
+        ps[2] = new Point(p.x - 1, 7 - p.y - 1);
+        ps[3] = new Point(p.x + 1, 7 - p.y);
+        ps[4] = new Point(p.x - 1, 7 - p.y);
+        ps[5] = new Point(p.x + 1, 7 - p.y + 1);
+        ps[6] = new Point(p.x, 7 - p.y + 1);
+        ps[7] = new Point(p.x - 1, 7 - p.y + 1);
         return ok(Json.toJson(ps));
+    }
+
+
+    public static Result MovePiece(int fromX, int fromY, int toX, int toY) {
+        /*Logger.info("from (" + fromX + ", " + fromY + ") to (" + toX + ", " + toY + ")\n");
+        Point from = new Point(fromX, fromY);
+        Point to = new Point(toX, toY);
+        boolean result = chessGame.MovePiece(from, to);
+        return ok(result);*/
+
+
+        return ok("true");
+    }
+
+
+    public static Result aiMakeSeqMove() {
+        /*Logger.info("AI is going to make a move");
+        List<Point> fromto = chessGame.aiMakeATurnSequential();
+        return ok(Json.toJson(fromto));*/
+
+        return ok();
+    }
+
+
+    public static Result aiMakeParMove() {
+        /*Logger.info("AI is going to make a move");
+        List<Point> fromto = chessGame.aiMakeATurnParallel();
+        return ok(Json.toJson(fromto));*/
+
+        return ok();
     }
 }
--- PlayChess/app/models/ChessGame.java
+++ PlayChess/app/models/ChessGame.java
@@ -15,7 +15,7 @@ public class ChessGame {
 	private ChessBoard chessBoard;
 
 
-	public ChessGame() 
+	public ChessGame()
         {
 		chessBoard = new ChessBoard();
 	}
@@ -25,7 +25,7 @@ public class ChessGame {
 	 * then return a list of possible point on the board that the piece at this xy location
 	 * can move to
 	 */
-	public List<Point> getPossibleMoves(int x, int y) 
+	public List<Point> getPossibleMoves(int x, int y)
         {
             Point pt = new Point(x,y);
             List<Move> moveList = MoveLogic.determineMoves(chessBoard.getBoardTile(pt), chessBoard);
@@ -38,14 +38,19 @@ public class ChessGame {
 	}
 
 
+	public boolean moveList(Point from, Point to) {
+		return true;
+	}
+
+
 	// This method tells ai to make a turn and apply it to the board permanently
-	public Move aiMakeATurnParallel(Side side) throws InterruptedException, ExecutionException 
-        {                
+	public Move aiMakeATurnParallel(Side side) throws InterruptedException, ExecutionException
+        {
 		//Tracks progress of 8 asynchronous threads.
 		ExecutorService pool = Executors.newFixedThreadPool(8);
 		//A set of Future objects, which manage returned values from the callable threads.
 		Set<Future<List<Move>>> set = new HashSet<Future<List<Move>>>();
-		
+
 		//Using threads, simultaniously scan through each column tallying a list of possible moves.
 		for(int i = 0; i < 8; i++) {
 			//Create threads of ParallelProcessing. Accepts a List<Move> object.
@@ -53,14 +58,14 @@ public class ChessGame {
 			//pool.submit(thread) returns a "future" object containing a list of moves from that collumn.
 			Future<List<Move>> future = pool.submit(myCallableThread);
 			//Add future object (which contains List<Move> to our set of thread return values.
-                        
+
 			set.add(future);
 		}
-		
+
 		//Transition Set to List
 		List<Move> availableMoves = new ArrayList<Move>();
 		//Iterate through all the future objects in our HashSet.
-		for(Future<List<Move>> futureMoveList : set) 
+		for(Future<List<Move>> futureMoveList : set)
                 {
 			//List.addAll(collection) adds all the elments of a collection, in this case, all the elements of a List<Move>
 			//contained within each future object.
@@ -68,12 +73,12 @@ public class ChessGame {
 			availableMoves.addAll(futureMoveList.get());
 		}
 		ParallelProcessing p = new ParallelProcessing(this.chessBoard,side);
-                return p.actualMove(side, chessBoard,availableMoves);		
+                return p.actualMove(side, chessBoard,availableMoves);
 	}
-	
-	public Move aiMakeATurnSequential(Side side) 
-        {		
-		AI ai = new AI();                
-		return ai.actualMove(side, this.chessBoard);			
+
+	public Move aiMakeATurnSequential(Side side)
+        {
+		AI ai = new AI();
+		return ai.actualMove(side, this.chessBoard);
 	}
 }
